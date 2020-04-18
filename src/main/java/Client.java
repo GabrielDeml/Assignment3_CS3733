3de5afc333eefba26deea3a933283dc3b769f6ff
darvy0 <nmdarveau@wpi.edu>
2020-04-18 11:36:09 -0400

    :/

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,6 +1,14 @@
 public class Client {
     private int _clientID;
     private String _clientName;
+    private Employee _salesEmployee;
+    private static int _clientCount = 0;
+
+    public Client(int clientID, String clientName, Employee salesEmployee) {
+        this._clientID = clientID;
+        this._clientName = clientName;
+        this._salesEmployee = salesEmployee;
+    }
 
     double totalSales() { return -1; }
 
@@ -12,11 +20,23 @@ public class Client {
         this._clientID = _clientID;
     }
 
+    public Employee get_salesEmployee() {
+        return _salesEmployee;
+    }
+
+    public void set_salesEmployee(Employee _salesEmployee) {
+        this._salesEmployee = _salesEmployee;
+    }
+
     public String get_clientName() {
         return _clientName;
     }
 
     public void set_clientName(String _clientName) {
         this._clientName = _clientName;
+    }
+
+    public static int get_clientCount() {
+        return _clientCount;
     }
 }
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,13 +2,21 @@ import java.util.ArrayList;
 
 public abstract class Employee {
     protected long salesID;
+    private ArrayList<Client> _clients;
 
     public Boolean addClient(String client) {
+        for (int i = 0; i <_clients.size(); i++) {
+            if (_clients.get(i).get_clientName().equals(client)) {
+                return false;
+            }
+        }
+        int clientCount = Client.get_clientCount();
+        _clients.add(new Client(clientCount+1, "name"+clientCount, this));
         return true;
     }
 
     private ArrayList<Client> getClients() {
-        return new ArrayList<Client>();
+        return _clients;
     }
 
     public long getSalesID() {
